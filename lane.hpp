59e94da8538af19103638bcd8b5fb2728b784e18
Jason Sewall <jasonsewall@gmail.com>
2008-12-03 20:39:09 -0500

    Working on a lane representation

---
+++ lane.hpp
@@ -0,0 +1,61 @@
+#include <vector>
+
+struct q
+{
+    float rho;
+    float y;
+};
+
+struct road
+{
+    const char * name;
+};
+
+typedef int lane_id;
+#define NO_LANE -1
+
+typedef int entry_id;
+
+struct lane_adjacency
+{
+    lane_id neighbor;
+    entry_id interval_entry;
+};
+
+typedef int intersection_id;
+#define TAPER    -2
+#define DEAD_END -1
+
+struct lane_end
+{
+    intersection_id intersection;
+};
+
+template <class T>
+struct intervals
+{
+    struct entry
+    {
+        float end;
+        T data;
+    };
+
+    std::vector<entry> entries;
+};
+
+typedef intervals<lane_adjacency> adjacency_intervals;
+
+struct lane
+{
+    adjacency_intervals left;
+    adjacency_intervals right;
+
+    lane_end start;
+    lane_end end;
+
+    float speedlimit;
+
+    float h;
+    unsigned int ncells;
+    q *data;
+};
---
+++ lanetest.cpp
@@ -0,0 +1,7 @@
+#include "lane.hpp"
+
+int main(int argc, char * argv[])
+{
+
+    return 0;
+}
