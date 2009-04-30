f0c0153e8378588402dc42c7cd961bcb83c1395a
Jason Sewall <jasonsewall@gmail.com>
2009-04-29 22:44:06 -0400

    Update car-data-test to read multiple cars
    Update build system (to include boost_filesystem and boost_regex)
    Update xml files to be correct

--- test-progs/car-data-test.cpp
+++ test-progs/car-data-test.cpp
@@ -1,16 +1,42 @@
 #include "car-models.hpp"
+#include <iostream>
+#include <boost/filesystem.hpp>
+#include <boost/regex.hpp>
+
+namespace bf = boost::filesystem;
 
 int main(int argc, char **argv)
 {
-    car_model cm;
-
-    if(argc < 2)
+    if(argc == 2)
     {
-        fprintf(stderr, "Usage: %s <xml-file>\n", argv[0]);
-        exit(1);
+        bf::path srcdir(argv[1]);
+        if(bf::is_directory(srcdir))
+        {
+            boost::regex re(".*\\.xml");
+            bf::directory_iterator end_itr;
+            for( bf::directory_iterator itr(srcdir);
+                 itr != end_itr;
+                 ++itr)
+            {
+                if(itr->path().has_filename() && boost::regex_match(itr->path().filename(), re))
+                {
+                    std::cout << "Loading " << itr->path().file_string() << "...";
+
+                    car_model cm;
+                    if(cm.load_from_xml(itr->path().file_string().c_str()))
+                        std::cout << "Done." << std::endl;
+                    else
+                    {
+                        std::cout << "Error" << std::endl;
+                        exit(1);
+                    }
+                }
+            }
+
+            return 0;
+        }
     }
+    fprintf(stderr, "Usage: %s <dir>\n", argv[0]);
 
-    cm.load_from_xml(argv[1]);
-
-    return 0;
+    return 1;
 }
