7ebe3078ac4b8f686809f186d056253a93ccf2c4
Jason Sewall <jasonsewall@gmail.com>
2009-09-10 23:51:59 -0400

    WIP on C++ bin-pack

--- etc/bin-pack.cpp
+++ etc/bin-pack.cpp
@@ -5,16 +5,19 @@
 #include <boost/foreach.hpp>
 #include <boost/range/iterator_range.hpp>
 #include <boost/assign/std/vector.hpp>
+#include <boost/utility.hpp>
 
-typedef std::vector<float>                    joblist;
-typedef std::vector<float>::iterator          job_iter;
-typedef std::vector<float>::const_iterator    const_job_iter;
-typedef boost::iterator_range<job_iter>       job_range;
-typedef boost::iterator_range<const_job_iter> const_job_range;
+typedef std::vector<float>                      joblist;
+typedef std::vector<float>::iterator            job_iter;
+typedef std::vector<float>::const_iterator      const_job_iter;
+typedef boost::iterator_range<job_iter>         job_range;
+typedef boost::iterator_range<const_job_iter>   const_job_range;
 
-typedef std::vector<joblist>               binlist;
+typedef std::vector<joblist>                    binlist;
 
 typedef std::vector<int>                   config;
+typedef config::iterator                   config_iter;
+typedef boost::iterator_range<config_iter> config_range;
 
 using namespace boost::assign;
 
@@ -133,8 +136,171 @@ void pack_small(binlist &bins, const_job_range &small_range)
     }
 }
 
-void minbins(const config &conf, float eps, float h)
+struct feasible_iterator
 {
+    feasible_iterator(const config &maxconf_,
+                      float base,
+                      float h) :
+        maxconf_(maxconf_),
+        base_(base), h_(h), left_(1.0f)
+    {
+    }
+
+    void reset()
+    {
+        left_ = 1.0f;
+    }
+
+    bool next(config &resconf)
+    {
+        int position = maxconf_.size()-1;
+        ++resconf[position];
+        left_ -= base_ + position*h_;
+        while(resconf[position] > maxconf_[position] || left_ < 0.0f)
+        {
+            left_ += resconf[position]*(base_ + position*h_);
+            resconf[position] = 0;
+            --position;
+            if(position < 0)
+                return false;
+            ++resconf[position];
+            left_ -= base_ + position*h_;
+        }
+        return true;
+    }
+
+    const config &maxconf_;
+    float base_;
+    float h_;
+    float left_;
+};
+
+struct bin_search
+{
+    bin_search()
+        : bins(-1), parent(0)
+    {}
+    bin_search(int b)
+        : bins(b), parent(0)
+    {}
+    bin_search(int b, bin_search *p)
+        : bins(b), parent(p)
+    {}
+
+    int bins;
+    bin_search *parent;
+};
+
+inline size_t index(const config &current, const config &dim)
+{
+    size_t sz = current.size();
+    size_t res = 0;
+    for(int i = 0; i < sz-1; ++i)
+    {
+        res = (res + current[i])*(dim[i+1]+1);
+    }
+    res += current[sz-1];
+
+    return res;
+}
+
+inline void unindex(config &res, size_t idx, const config &cum_dims)
+{
+    size_t sz = res.size();
+
+    for(int i = 0; i < sz-1; ++i)
+    {
+        res[i] = idx/cum_dims[i];
+        idx -= res[i]*cum_dims[i];
+    }
+
+    res[sz-1] = idx;
+}
+
+std::vector<config> minbins(const config &conf, float eps, float h)
+{
+    size_t prod = 1;
+    BOOST_FOREACH(const int &i, conf)
+    {
+        prod *= (i+1);
+    }
+
+    bin_search *search = new bin_search[prod];
+
+    std::vector<config> todo;
+    feasible_iterator fi(conf, eps, h);
+    {
+        config res(conf.size());
+        std::fill(res.begin(), res.end(), 0);
+        while(fi.next(res))
+            todo.push_back(res);
+    }
+
+    BOOST_FOREACH(const config &c, todo)
+    {
+        search[index(c, conf)] = bin_search(1);
+    }
+
+    while(!todo.empty())
+    {
+        config t0    (todo.back());
+        todo.pop_back();
+        size_t t0_idx(index(t0, conf));
+
+        config tf(conf);
+        for(int i = 0; i < tf.size(); ++i)
+            tf[i] -= t0[i];
+
+        feasible_iterator fi(tf, eps, h);
+        {
+            config res(conf.size());
+            std::fill(res.begin(), res.end(), 0);
+            while(fi.next(res))
+            {
+                config res_total(res);
+                for(int i = 0; i < conf.size(); ++i)
+                    res_total[i] += t0[i];
+
+                size_t t1_idx = index(res_total, conf);
+                if( search[t1_idx].bins == -1 ||
+                    search[t0_idx].bins + 1 < search[t1_idx].bins)
+                {
+                    search[t1_idx] = bin_search(1 + search[t0_idx].bins, search + t0_idx);
+                    todo.push_back(res_total);
+                }
+            }
+        }
+    }
+
+    config cum_dim(conf.size()-1);
+    cum_dim.back() = conf[conf.size()-1]+1;
+    for(int i = cum_dim.size()-2; i >= 0; --i)
+        cum_dim[i] = cum_dim[i+1]*(conf[i+1]+1);
+
+    std::vector<config> result;
+    bin_search *last = search + index(conf, conf);
+    config last_conf(conf);
+
+    bin_search *current = last->parent;
+    config current_conf(conf.size());
+    unindex(current_conf, current - search, cum_dim);
+
+    while(current)
+    {
+        result.push_back(config(conf.size()));
+        for(int i = 0; i < conf.size(); ++i)
+            result.back()[i] = last_conf[i] - current_conf[i];
+        last = current;
+        std::swap(last_conf, current_conf);
+        current = last->parent;
+        if(current)
+            unindex(current_conf, current - search, cum_dim);
+    }
+    result.push_back(last_conf);
+
+    delete search;
+
+    return result;
 }
 
 int main(int argc, char *argv[])
@@ -197,6 +363,22 @@ int main(int argc, char *argv[])
         std::cout << "]";
     }
     std::cout << std::endl;
+
+    config maxconf;
+    maxconf += 2, 3, 5;
+
+    std::vector<config> res = minbins(maxconf, 0.2, 0.2);
+    BOOST_FOREACH(const config &cf, res)
+    {
+        std::cout << "[ ";
+        BOOST_FOREACH(const float &c, cf)
+        {
+            std::cout << c << " ";
+        }
+        std::cout << "]";
+    }
+    std::cout << std::endl;
+
 
     return 0;
 }
