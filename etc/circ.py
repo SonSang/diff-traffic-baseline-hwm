736228af31f2dd7671fd107b92d204f5528db07a
Jason Sewall <jasonsewall@gmail.com>
2009-09-28 11:51:08 -0400

    Fix circle function to behave correctly
    I think this is correct now

--- etc/circ.py
+++ etc/circ.py
@@ -1,12 +1,13 @@
 import math
 
-def circle2(radius, ang_range, nsteps, center, direction):
-    #direction = -1 # cw
-    ra2 = [ math.fmod(x + 2*math.pi, 2*math.pi) for x in ang_range ]
-    if direction == 1:
-        dist = ra2[1] - ra2[0]
-    else:
-        dist = (2*math.pi-ra2[1]) + (2*math.pi-ra2[0])
-    for i in xrange(nsteps):
-        ang = direction*(ra2[0] + dist*i/float(nsteps-1))
-        print radius*math.cos(ang) + center[0], radius*math.sin(ang) + center[1], 0.0, 0.0
+def circle(center, radius, angint, ccw, nsteps):
+    angint2 = [ math.fmod(x + 2*math.pi, 2*math.pi) for x in angint ]
+    inorder = angint2[0] <= angint2[1]
+    adist = abs(angint2[1] - angint2[0])
+    if (not ccw and inorder) or (ccw and not inorder):
+        adist = 2*math.pi - adist
+    if not ccw:
+        adist *= -1
+    steps = ( math.fmod(angint2[0] + (adist)*x/float(nsteps-1), 2*math.pi) for x in xrange(nsteps) )
+    return ( (radius*math.cos( theta ) + center[0], radius*math.sin( theta ) + center[1]) for theta in steps )
+
