116b9338a8e75c870dc44ed0e44560d8af3cf986
Jason Sewall <jasonsewall@gmail.com>
2009-09-28 09:45:29 -0400

    Update circle2 computation of arcs of circles
    Not quite right, but better

--- etc/circ.py
+++ etc/circ.py
@@ -1,9 +1,12 @@
 import math
 
-def circle2(radius, ang_range, nsteps, center):
+def circle2(radius, ang_range, nsteps, center, direction):
+    #direction = -1 # cw
+    ra2 = [ math.fmod(x + 2*math.pi, 2*math.pi) for x in ang_range ]
+    if direction == 1:
+        dist = ra2[1] - ra2[0]
+    else:
+        dist = (2*math.pi-ra2[1]) + (2*math.pi-ra2[0])
     for i in xrange(nsteps):
-        ang = ang_range[0] + (ang_range[1]-ang_range[0])*i/float(nsteps-1)
-        print radius*math.cos(ang) + center[0], radius*math.sin(ang) + center[1]
-
-
-
+        ang = direction*(ra2[0] + dist*i/float(nsteps-1))
+        print radius*math.cos(ang) + center[0], radius*math.sin(ang) + center[1], 0.0, 0.0
