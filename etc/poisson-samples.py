f68287dc0772c0bbde87f9f133bb56c9e1407016
Jason Sewall <jasonsewall@gmail.com>
2010-03-11 11:04:36 -0500

    Add plotting of poisson samples (thinning method)

--- etc/poisson-samples.py
+++ etc/poisson-samples.py
@@ -4,6 +4,9 @@ import math
 import random
 import itertools as it
 import bisect
+import numpy
+import pylab
+import matplotlib
 
 def exp_pdf(x, lam):
     return lam*math.exp(-lam*x)
@@ -52,6 +55,14 @@ class cap(object):
     def inv_integral(self, t):
         return t
 
+class ecap(object):
+    def value(self, t):
+        return math.exp(t)
+    def integral(self, t):
+        return math.exp(t)
+    def inv_integral(self, t):
+        return math.log(t)
+
 def lam(x):
     vals      = [0.5]
     intervals = [50.0]
@@ -61,7 +72,7 @@ def lam(x):
         return vals[p]
     raise Exception("Undefined lambda value")
 
-def thinning_poisson(limit, lam, cap_obj):
+def thinning_poisson_car(limit, lam, cap_obj):
     t = 0.25
     k = 0
     while True:
@@ -76,15 +87,49 @@ def thinning_poisson(limit, lam, cap_obj):
             yield Z
 
 def test():
-    gen = thinning_poisson(50, lam, cap())
+    gen = thinning_poisson_car(50, lam, cap())
     try:
         while True:
             print gen.next()
     except StopIteration:
         pass
 
+def show_poisson_proc(ax, start_end, lam, cap_obj):
+    gen = thinning_poisson(start_end[0], start_end[1], lam, cap_obj)
+    res = []
+    try:
+        while True:
+            res.append(gen.next())
+    except StopIteration:
+        pass
 
+    x = numpy.linspace(start_end[0], start_end[1])
+    y = numpy.array([lam(i) for i in x])
+    ax.plot(x, y)
+    maxy = max(y)
+    for i in res:
+        l0 = matplotlib.lines.Line2D((i, i), (0, maxy), color='black', linewidth=1.0)
+        ax.add_line(l0)
 
+    return ax
+
+def thinning_poisson(start, limit, lam, cap_obj):
+    t = start
+    k = 0
+    while True:
+        Z = inhomo_poisson_inv(t, cap_obj).next()
+        U = random.random()
+        t = Z
+        if t > limit:
+            return
+        if U <= lam(Z)/cap_obj.value(Z):
+            k += 1
+            yield Z
+
+if __name__ == '__main__':
+    pylab.clf()
+    show_poisson_proc(pylab.axes(), (0, 2), lambda x: math.exp(x), ecap())
+    pylab.show()
 
 
 
