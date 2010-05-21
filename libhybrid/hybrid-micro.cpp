576a333476a5a2aeacf90403dac6428cf065b91b
Jason Sewall <jasonsewall@gmail.com>
2010-05-21 12:35:59 -0400

    Re-indent micro code

--- libhybrid/hybrid-micro.cpp
+++ libhybrid/hybrid-micro.cpp
@@ -473,39 +473,39 @@ namespace hybrid
 
     void lane::compute_lane_accelerations(const float timestep, const simulator &sim)
     {
-         if(current_cars().empty())
-             return;
+        if(current_cars().empty())
+            return;
 
-         for(size_t i = 0; i < current_cars().size(); ++i)
-         {
-             if (i < current_cars().size() - 1)
-                 current_car(i).compute_acceleration(current_car(i+1), (current_car(i+1).position - current_car(i).position)*length, sim);
-             else
-                 current_cars().back().compute_intersection_acceleration(sim, *this);
+        for(size_t i = 0; i < current_cars().size(); ++i)
+        {
+            if (i < current_cars().size() - 1)
+                current_car(i).compute_acceleration(current_car(i+1), (current_car(i+1).position - current_car(i).position)*length, sim);
+            else
+                current_cars().back().compute_intersection_acceleration(sim, *this);
 
-             // //Check if there are cars still merging out of this lane.
-             float right_param = current_car(i).position;
-             hwm::lane* potential_right = parent->right_adjacency(right_param);
-             lane* right_lane;
-             float right_accel = 0;
-             car next_r;
-             if (potential_right)
-             {
+            // //Check if there are cars still merging out of this lane.
+            float right_param = current_car(i).position;
+            hwm::lane* potential_right = parent->right_adjacency(right_param);
+            lane* right_lane;
+            float right_accel = 0;
+            car next_r;
+            if (potential_right)
+            {
                 right_lane = potential_right->user_data<lane>();
 
                 next_r  = right_lane->get_merging_leader(right_param, this);
 
                 if (next_r.position > -1)
                     right_accel = sim.acceleration(next_r.velocity, current_car(i).velocity, std::abs(next_r.position - right_param)*right_lane->length);
-             }
+            }
 
-             float left_param = current_car(i).position;
-             hwm::lane* potential_left = parent->left_adjacency(left_param);
-             lane* left_lane;
-             float left_accel = 0;
-             car next_l;
-             if (potential_left)
-             {
+            float left_param = current_car(i).position;
+            hwm::lane* potential_left = parent->left_adjacency(left_param);
+            lane* left_lane;
+            float left_accel = 0;
+            car next_l;
+            if (potential_left)
+            {
                 left_lane = potential_left->user_data<lane>();
 
                 next_l  = left_lane->get_merging_leader(left_param, this);
@@ -513,170 +513,170 @@ namespace hybrid
                 {
                     left_accel = sim.acceleration(next_l.velocity, current_car(i).velocity, std::abs(next_l.position - left_param)*left_lane->length);
                 }
-             }
+            }
 
-             if (potential_right and next_r.position > -1)
-                 current_car(i).acceleration = std::min(current_car(i).acceleration, (float)right_accel);
+            if (potential_right and next_r.position > -1)
+                current_car(i).acceleration = std::min(current_car(i).acceleration, (float)right_accel);
 
-             if (potential_left and next_l.position > -1)
-                 current_car(i).acceleration = std::min(current_car(i).acceleration, (float)left_accel);
-         }
-     }
+            if (potential_left and next_l.position > -1)
+                current_car(i).acceleration = std::min(current_car(i).acceleration, (float)left_accel);
+        }
+    }
 
-     float lane::settle_pass(const float timestep, const float epsilon, const float epsilon_2,
-                              const simulator &sim)
-     {
-         float max_acceleration = epsilon;
-         int i = static_cast<int>(current_cars().size())-1;
-         while(!current_cars().empty() && i >= 0)
-         {
-             car &c = current_car(i);
+    float lane::settle_pass(const float timestep, const float epsilon, const float epsilon_2,
+                            const simulator &sim)
+    {
+        float max_acceleration = epsilon;
+        int i = static_cast<int>(current_cars().size())-1;
+        while(!current_cars().empty() && i >= 0)
+        {
+            car &c = current_car(i);
 
-             float last_acceleration = std::numeric_limits<float>::max();
-             while(true)
-             {
-                 compute_lane_accelerations(timestep, sim);
+            float last_acceleration = std::numeric_limits<float>::max();
+            while(true)
+            {
+                compute_lane_accelerations(timestep, sim);
 
-                 c.velocity = std::max(c.velocity + c.acceleration * timestep,
-                                        0.0f);
+                c.velocity = std::max(c.velocity + c.acceleration * timestep,
+                                      0.0f);
 
-                 max_acceleration = std::max(std::abs(c.acceleration), max_acceleration);
+                max_acceleration = std::max(std::abs(c.acceleration), max_acceleration);
 
-                 if( std::abs(c.acceleration) > std::abs(last_acceleration)
-                     or c.position+sim.front_bumper_offset()*inv_length >= 1.0
-                     or ((std::abs(c.acceleration - last_acceleration) < epsilon_2) and (std::abs(c.acceleration) > epsilon)))
-                 {
-                     for(int j = i; j < static_cast<int>(current_cars().size())-1; ++j)
-                         current_car(j) = current_car(j+1);
-                     current_cars().pop_back();
-                     break;
-                 }
-                 else if(std::abs(c.acceleration) < epsilon)
-                 {
-                     --i;
-                     break;
-                 }
+                if( std::abs(c.acceleration) > std::abs(last_acceleration)
+                    or c.position+sim.front_bumper_offset()*inv_length >= 1.0
+                    or ((std::abs(c.acceleration - last_acceleration) < epsilon_2) and (std::abs(c.acceleration) > epsilon)))
+                {
+                    for(int j = i; j < static_cast<int>(current_cars().size())-1; ++j)
+                        current_car(j) = current_car(j+1);
+                    current_cars().pop_back();
+                    break;
+                }
+                else if(std::abs(c.acceleration) < epsilon)
+                {
+                    --i;
+                    break;
+                }
 
-                 last_acceleration = c.acceleration;
-             }
-         }
-         return max_acceleration;
-     }
+                last_acceleration = c.acceleration;
+            }
+        }
+        return max_acceleration;
+    }
 
-     void simulator::micro_initialize(const float in_a_max, const float in_a_pref, const float in_v_pref,
-                                      const float in_delta)
-     {
-         a_max                 = in_a_max;
-         a_pref                = in_a_pref;
-         v_pref                = in_v_pref;
-         delta                 = in_delta;
-     }
+    void simulator::micro_initialize(const float in_a_max, const float in_a_pref, const float in_v_pref,
+                                     const float in_delta)
+    {
+        a_max                 = in_a_max;
+        a_pref                = in_a_pref;
+        v_pref                = in_v_pref;
+        delta                 = in_delta;
+    }
 
-     void simulator::settle(const float timestep)
-     {
-         static const float EPSILON   = 1;
-         static const float EPSILON_2 = 0.01;
+    void simulator::settle(const float timestep)
+    {
+        static const float EPSILON   = 1;
+        static const float EPSILON_2 = 0.01;
 
-         float max_acceleration;
-         do
-         {
-             max_acceleration = EPSILON;
-             BOOST_FOREACH(lane &l, lanes)
-             {
-                 if(l.is_micro() && l.parent->active)
-                     max_acceleration = std::max(l.settle_pass(timestep, EPSILON, EPSILON_2, *this),
-                                                 (float) max_acceleration);
-             }
+        float max_acceleration;
+        do
+        {
+            max_acceleration = EPSILON;
+            BOOST_FOREACH(lane &l, lanes)
+            {
+                if(l.is_micro() && l.parent->active)
+                    max_acceleration = std::max(l.settle_pass(timestep, EPSILON, EPSILON_2, *this),
+                                                (float) max_acceleration);
+            }
 
-             std::cout << "Max acceleration in settle: " << max_acceleration << std::endl;
-         }
-         while(max_acceleration > EPSILON);
-     }
+            std::cout << "Max acceleration in settle: " << max_acceleration << std::endl;
+        }
+        while(max_acceleration > EPSILON);
+    }
 
-     float simulator::acceleration(const float leader_velocity, const float follower_velocity, float distance) const
-     {
-         static const float s1 = 2;
-         static const float T  = 1.6;
-         const float EPSILON = 10e-7;
+    float simulator::acceleration(const float leader_velocity, const float follower_velocity, float distance) const
+    {
+        static const float s1 = 2;
+        static const float T  = 1.6;
+        const float EPSILON = 10e-7;
 
-         //if (f.vel < 0) f.vel = 0; //TODO Should this be taken into account?
+        //if (f.vel < 0) f.vel = 0; //TODO Should this be taken into account?
 
-         const float optimal_spacing = s1 + T*follower_velocity + (follower_velocity*(follower_velocity - leader_velocity))/2*(std::sqrt(a_max*a_pref));
+        const float optimal_spacing = s1 + T*follower_velocity + (follower_velocity*(follower_velocity - leader_velocity))/2*(std::sqrt(a_max*a_pref));
 
-         if (distance > car_length)
-             distance -= car_length;
-         if (distance <= 0)
-             distance = EPSILON;
+        if (distance > car_length)
+            distance -= car_length;
+        if (distance <= 0)
+            distance = EPSILON;
 
-         //         std::cout << "leader vel " << leader_velocity << " dist " << distance << std::endl;
-         const float t               = a_max*(1 - std::pow((follower_velocity / v_pref), delta) - std::pow((optimal_spacing/(distance)), 2));
+        //         std::cout << "leader vel " << leader_velocity << " dist " << distance << std::endl;
+        const float t               = a_max*(1 - std::pow((follower_velocity / v_pref), delta) - std::pow((optimal_spacing/(distance)), 2));
 
-         //assert(l.dist - f.dist > 0); //A leader needs to lead.
-         return t;
-     }
+        //assert(l.dist - f.dist > 0); //A leader needs to lead.
+        return t;
+    }
 
-     void simulator::compute_accelerations(const float timestep)
-     {
-         BOOST_FOREACH(lane &l, lanes)
-         {
-             if (l.is_micro() && l.parent->active)
-                 l.compute_lane_accelerations(timestep, *this);
-         }
+    void simulator::compute_accelerations(const float timestep)
+    {
+        BOOST_FOREACH(lane &l, lanes)
+        {
+            if (l.is_micro() && l.parent->active)
+                l.compute_lane_accelerations(timestep, *this);
+        }
 
-         BOOST_FOREACH(lane& l, lanes)
-         {
-             if (l.is_micro() and l.parent->active)
-                 l.compute_merges(timestep, *this);
-         }
+        BOOST_FOREACH(lane& l, lanes)
+        {
+            if (l.is_micro() and l.parent->active)
+                l.compute_merges(timestep, *this);
+        }
 
-         BOOST_FOREACH(lane& l, lanes)
-         {
-             l.car_swap();
-         }
-     }
+        BOOST_FOREACH(lane& l, lanes)
+        {
+            l.car_swap();
+        }
+    }
 
-     void simulator::update(const float timestep)
-     {
-         compute_accelerations(timestep);
+    void simulator::update(const float timestep)
+    {
+        compute_accelerations(timestep);
 
-         BOOST_FOREACH(lane &l, lanes)
-         {
-             if(l.is_micro() && l.parent->active)
-             {
-                 BOOST_FOREACH(car &c, l.current_cars())
-                 {
-                     c.integrate(timestep, l, hnet->lane_width);
-                 }
-             }
-         }
+        BOOST_FOREACH(lane &l, lanes)
+        {
+            if(l.is_micro() && l.parent->active)
+            {
+                BOOST_FOREACH(car &c, l.current_cars())
+                {
+                    c.integrate(timestep, l, hnet->lane_width);
+                }
+            }
+        }
 
-         BOOST_FOREACH(lane &l, lanes)
-         {
-             if(l.is_micro() && l.parent->active)
-             {
-                 BOOST_FOREACH(car &c, l.current_cars())
-                 {
-                     lane* destination_lane = &l;
-                     lane* curr = &l;
+        BOOST_FOREACH(lane &l, lanes)
+        {
+            if(l.is_micro() && l.parent->active)
+            {
+                BOOST_FOREACH(car &c, l.current_cars())
+                {
+                    lane* destination_lane = &l;
+                    lane* curr = &l;
 
-                     while(c.position >= 1.0)
-                     {
-                         if(curr->parent->end->network_boundary())
-                             goto next_car;
+                    while(c.position >= 1.0)
+                    {
+                        if(curr->parent->end->network_boundary())
+                            goto next_car;
 
-                         hwm::lane *hwm_downstream = curr->parent->downstream_lane();
-                         assert(hwm_downstream);
-                         assert(hwm_downstream->active);
+                        hwm::lane *hwm_downstream = curr->parent->downstream_lane();
+                        assert(hwm_downstream);
+                        assert(hwm_downstream->active);
 
-                         lane* downstream = hwm_downstream->user_data<lane>();
+                        lane* downstream = hwm_downstream->user_data<lane>();
 
-                         switch(downstream->sim_type)
-                         {
-                         case MICRO:
-                             c.position = (c.position - 1.0f) * curr->length * downstream->inv_length;
-                             // downstream->next_cars().push_back(c);
-                             destination_lane = downstream;
-                             break;
+                        switch(downstream->sim_type)
+                        {
+                        case MICRO:
+                            c.position = (c.position - 1.0f) * curr->length * downstream->inv_length;
+                            // downstream->next_cars().push_back(c);
+                            destination_lane = downstream;
+                            break;
                         case MACRO: //TODO update for correctness
                             downstream->q[0].rho() = std::min(1.0f, downstream->q[0].rho() + car_length/downstream->h);
                             downstream->q[0].y()   = std::min(0.0f, arz<float>::eq::y(downstream->q[0].rho(), c.velocity,
@@ -696,7 +696,7 @@ namespace hybrid
 
                     destination_lane->next_cars().push_back(c);
 
-                 next_car:;
+                next_car:;
                 }
             }
         }
